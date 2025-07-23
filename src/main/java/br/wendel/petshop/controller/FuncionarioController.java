0f38bea295a48a9c629880a60885116fdb57947c
Wendel <jwend3l@gmail.com>
2025-07-23 00:25:38 -0300

    ClienteController básico adicionado

---
+++ src/main/java/br/wendel/petshop/controller/FuncionarioController.java
@@ -0,0 +1,42 @@
+package br.wendel.petshop.controller;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.wendel.petshop.entity.Funcionario;
+import br.wendel.petshop.repository.FuncionarioRepository;
+
+@RestController
+@RequestMapping("/funcionarios")
+public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    @GetMapping
+    public List<Funcionario> listarFuncionarios(){
+        return funcionarioRepository.findAll();
+    }
+
+    @GetMapping("/{cpf}")
+    public Optional<Funcionario> buscarPorId(@PathVariable String id){
+        return funcionarioRepository.findById(id);
+    }
+
+    @PostMapping
+    public Funcionario cadastrarFuncionario(@RequestBody Funcionario funcionario){
+        return funcionarioRepository.save(funcionario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarCliente(@PathVariable String id){
+        funcionarioRepository.deleteById(id);
+    }
+
+}
--- src/main/java/br/wendel/petshop/entity/Funcionario.java
+++ src/main/java/br/wendel/petshop/entity/Funcionario.java
@@ -11,8 +11,8 @@ import br.wendel.petshop.enums.Cargo;
 public class Funcionario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Pattern(regexp = "\\d{11}", message = "CPF deve conter 11 dígitos")
+    private String id;
 
     @NotBlank(message = "Nome Obrigatório")
     private String nome;
@@ -26,16 +26,15 @@ public class Funcionario {
 
     public Funcionario() {}
 
-    public Funcionario(Long id, String nome, String cpf, Cargo cargo) {
+    public Funcionario(String id, String nome, String cpf, Cargo cargo) {
         this.id = id;
         this.nome = nome;
         this.cpf = cpf;
         this.cargo = cargo;
     }
 
-    // Getters e setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
 
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
